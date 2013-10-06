54471b0764c5ee77d708b04addf835904e0a9352
Steve Dorazio <sdorazio@50onred.com>
2013-10-06 14:42:27 -0400

    Changing default location update time/distance

--- WanderSafe/WanderSafe/src/main/java/com/fiftyonred/wandersafe/service/WandersafeService.java
+++ WanderSafe/WanderSafe/src/main/java/com/fiftyonred/wandersafe/service/WandersafeService.java
@@ -42,7 +42,7 @@ public class WandersafeService extends Service implements LocationListener {
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
         //Register for GPS updates
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, this);
 
         locationProvider = new LocationProvider(locationManager);
 
